f25c74568b95a5e24ba9e782ad217f3efc460be5
kmabasa212 <kmabasa212@gmail.com>
2024-04-10 09:29:53 +0200

    static web app cors

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 const cors = require('cors');
 
 app.use(cors({
-    origin: 'https://testing-apiii.azurewebsites.net'
+    origin: 'https://blue-flower-08292cd03.5.azurestaticapps.net/'
 }));
 
 app.use(express.json());
