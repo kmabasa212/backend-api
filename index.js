55c47fa23b0b9005d7a8b05eefc156e90d6e3549
kmabasa212 <kmabasa212@gmail.com>
2024-04-10 18:22:02 +0200

    do your thing

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const cors = require('cors');
+const helmet = require('helmet');
 
 
    /* app.use(cors({
@@ -11,7 +12,7 @@ const cors = require('cors');
 
     app.use(cors());
     
-
+    app.use(helmet());
 
 app.use(express.json());
 
